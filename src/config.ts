24a9065f44cb871dd203e562a2887813ee06b109
pengzhaofeng <pengzhaofeng@safeheron.com>
2023-10-19 18:46:43 +0800

    auto sweep

--- src/config.ts
+++ src/config.ts
@@ -13,6 +13,6 @@ export interface SafeheronCoSignerConfig {
 }
 
 export interface SafeheronWebHookConfig {
-  safeheronWebHookRsaPublicKey: string;
   webHookRsaPrivateKey: string;
+  safeheronWebHookRsaPublicKey: string;
 }
